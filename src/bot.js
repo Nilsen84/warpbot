1b211c0dfc4ee2e71e03490922e402c259f12298
Nilsen84 <56961087+nilsen84@users.noreply.github.com>
2022-07-06 22:45:12 +0200

    Fix for windows

--- src/bot.js
+++ src/bot.js
@@ -1,46 +1,50 @@
 import {createBot} from 'mineflayer'
 
-const bot = createBot({
-    host: 'hypixel.net',
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    auth: 'microsoft',
-    version: '1.8.9'
-})
+export default class {
+    constructor(username, password) {
+        this.bot = createBot({
+            host: 'hypixel.net',
+            username: username,
+            password: password,
+            auth: 'microsoft',
+            version: '1.8.9'
+        })
 
-bot.once('spawn', () => {
-    setInterval(async () => {
-        bot.setControlState('jump', true)
-        await bot.waitForTicks(11)
-        bot.clearControlStates()
-    }, 10000)
-})
+        this.bot.once('spawn', () => {
+            setInterval(async () => {
+                this.bot.setControlState('jump', true)
+                await this.bot.waitForTicks(11)
+                this.bot.clearControlStates()
+            }, 10000)
+        })
 
-export function warp(plr){
-    bot.chat(`/party invite ${plr}`)
-}
+        this.bot.on('message', (msg) => {
+            let messages = [msg]
+            if(msg.extra != null) messages.push(...msg.extra)
 
-bot.on('message', (msg) => {
-    let messages = [msg]
-    if(msg.extra != null) messages.push(...msg.extra)
+            let nonWhite = messages.filter(e =>
+                e.color !== 'white'
+            ).map(e => e.text)
 
-    let nonWhite = messages.filter(e =>
-        e.color !== 'white'
-    ).map(e => e.text)
+            if(nonWhite.includes('joined the party.')){
+                this.bot.chat('/p warp')
+            }else if(nonWhite.includes('Some players are still in-game, run the command again to confirm warp!')){
+                setTimeout(() => {
+                    this.bot.chat('/p warp')
+                }, 500)
+            }else if(nonWhite.includes('You summoned ')){
+                setTimeout(() => {
+                    this.bot.chat('/p disband')
+                }, 500)
+            }
+        })
 
-    if(nonWhite.includes('joined the party.')){
-        bot.chat('/p warp')
-    }else if(nonWhite.includes('Some players are still in-game, run the command again to confirm warp!')){
-        setTimeout(() => {
-            bot.chat('/p warp')
-        }, 500)
-    }else if(nonWhite.includes('You summoned ')){
-        setTimeout(() => {
-            bot.chat('/p disband')
-        }, 500)
+        this.bot.on('message', (msg) => {
+            console.log(msg.toAnsi())
+        })
     }
-})
 
-bot.on('message', (msg) => {
-    console.log(msg.toAnsi())
-})
+    warp(plr){
+        this.bot.chat(`/party invite ${plr}`)
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-import * as bot from './bot.js';
+import WarpBot from './bot.js';
 import {Tail} from "tail";
 
-const opponentRegex = /\[Client thread\/INFO]: \[CHAT] \s+Opponent:.* (.+)$/
+const bot = new WarpBot(
+    process.env.EMAIL,
+    process.env.PASSWORD
+)
 
-const tail = new Tail(process.env.LOGFILE)
+const opponentRegex = /\[Client thread\/INFO]: \[CHAT] \s+Opponent:.* (.+)$/
+const tail = new Tail(process.env.LOGFILE, {
+    useWatchFile: process.platform === 'win32'
+})
 
 tail.on('line', line => {
     const match = opponentRegex.exec(line)
